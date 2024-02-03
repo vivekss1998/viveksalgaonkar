1e9cde2cb5b8d416d409e4a84c2f72ce7a038f06
vivekss1998 <viveksalgaonkar83@gmail.com>
2024-02-03 16:38:49 +0530

    swal-message

--- assets/js/script.js
+++ assets/js/script.js
@@ -214,15 +214,15 @@ document.onkeydown = function (e) {
 // Start of Tawk.to Live Chat
 
 // End of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/65769fcb07843602b8005dd8/1hhbm0d93';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/65769fcb07843602b8005dd8/1hhbm0d93';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
